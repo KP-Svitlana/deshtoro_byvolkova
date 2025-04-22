06a53b2d305dd8b8150c9d54f60df3ced55d36a0
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-04-22 18:36:42 +0300

    apdate about section markup and css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,24 +4,29 @@ import about2 from "../../accets/img/about2_x3_desktop.jpg";
 
 export const About = () => {
   return (
-    <section className="section"> 
+    <section className="section container">
       <h1 className="section_title">про нас</h1>
-      <h2>Стиль</h2>
-      <h2>Якість</h2>
-      <h2>Преміальність</h2>
-      <Image
-        src={about1}
-        alt="штора"
-        width={410}
-        height={528}
-      />
-      <Image
-        src={about2}
-        alt="тканина"
-        width={410}
-        height={312}
-      />
-      <p>
+      <ul className="about_list">
+        <li className="about_item">
+          <p>Стиль</p>
+        </li>
+        <li className="about_item">
+          <p>Якість</p>
+        </li>
+        <li className="about_item">
+          <p>Преміальність</p>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Image src={about1} alt="штора" width={410} height={528} />
+        </li>
+        <li>
+          <Image src={about2} alt="тканина" width={410} height={312} />
+        </li>
+      </ul>
+
+      <p className="about_text">
         Де ШТОРИ — це студія штор, де поєднуються стиль, якість та
         індивідуальний підхід. Ми створюємо штори, які ідеально доповнюють ваш
         інтер’єр та підкреслюють ваш смак. Від елегантної класики до сучасного
