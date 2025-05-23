65c2382f4fa041bf7c303d52ae063fa8a4b9c91b
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-05-23 22:45:11 +0300

    update hero css

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,8 +2,7 @@ import Video from "next-video";
 
 import { LogoSwitcherImg } from "../LogoSwitcherImg";
 
-import myVideo from "../../../videos/get-started.mp4.json";
-
+import myVideo from "../../../videos/mov_hero_desktop.mp4.json";
 
 import name_desktop from "@/accets/svg/name_hero_desktop.svg";
 import name_mobile from "@/accets/svg/name_hero_mobile.svg";
@@ -11,20 +10,28 @@ import name_mobile from "@/accets/svg/name_hero_mobile.svg";
 export const Hero = () => {
   return (
     <section className="hero">
-      <Video
-        src={myVideo}
-        autoplay
-        muted
-        loop
-        playsInLine
-        controls={false}
-        className={"hero_video"}
-      />
-      <div className="hero_titleWrap">
-        <LogoSwitcherImg logo_desktop={name_desktop} logo_mobile={name_mobile} />
-      <h1 className="hero_title">Індивідуальний дизайн штор для вашого дому</h1>
+      <div className="hero_videoWrapper">
+        {" "}
+        <Video
+          src={myVideo}
+          autoplay
+          muted
+          loop
+          playsInLine
+          controls={false}
+          className={"hero_video"}
+        />
       </div>
-    
+
+      <div className="hero_titleWrap">
+        <LogoSwitcherImg
+          logo_desktop={name_desktop}
+          logo_mobile={name_mobile}
+        />
+        <h1 className="hero_title">
+          Індивідуальний дизайн штор для вашого дому
+        </h1>
+      </div>
     </section>
   );
 };
