fc4f438c173425fd559ab8ccc13173353375ac3a
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-05-29 23:40:29 +0300

    updated header visual

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import errowSvg from "@/accets/svg/arrow.png";
 
-export const Button = ({ text, isErrow, type, path = "/" }) => {
+export const Button = ({ text, isErrow, type, path = "/"}) => {
   return (
     <>
       {type ? (
-        <button type={type} className="button">
+        <button type={type} className= "button">
           {text}
           {isErrow && <Image src={errowSvg} alt="Errow" />}
         </button>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 import { Button } from "../Button";
 import { LangSwitcher } from "../LangSwitcher";
 import { Navigate } from "../Navigate";
@@ -8,6 +9,8 @@ import { LogoSwitcherImg } from "../LogoSwitcherImg";
 
 import logo_desktop from "@/accets/svg/logo_header_desktop.png";
 import logo_mobile from "@/accets/svg/logo_header_mobile.png";
+import menu_mobile from "@/accets/svg/header_mobile_menu.svg";
+import phone_mobile from "@/accets/svg/header_mobile_phone.svg";
 
 export const Header = () => {
   const pathname = usePathname();
@@ -18,8 +21,12 @@ export const Header = () => {
       className={isHome ? "container header" : "container header header--color"}>
       <LogoSwitcherImg logo_desktop={logo_desktop} logo_mobile={logo_mobile} />
       <div className="header_wrapp">
-        <Navigate />
+        <Navigate className={"header_nav"}/>
         <LangSwitcher />
+      </div>
+      <div className="mobileIcon_wrapp">
+        <Image src={menu_mobile} alt={"menu icon"} width={44} height={44}/>
+        <Image src={phone_mobile} alt={"phone icon"} width={24} height={24} className="phone_icon"/>
       </div>
       <Button text={"Зв'язатися"} isErrow={false} type={"button"} />
     </header>
