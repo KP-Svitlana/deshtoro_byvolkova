447a774ca128b9ca022161c9e5e1cde877adb38d
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-06-11 13:18:53 +0300

    add aboutUs data and some css

--- src/app/aboutUs/page.js
+++ src/app/aboutUs/page.js
@@ -1,3 +1,24 @@
+import Image from "next/image";
+
+import { aboutUsData } from "@/constants";
+
+
 export default function AboutUs() {
-  return <h1>AboutUs</h1>;
+  return (
+    <main>
+      <section className="container aboutUs_section">
+        <ul>
+          {aboutUsData.map((item) => {
+            return (
+              <li key={item.label} >
+                <Image src={item.src} alt={item.label} className="aboutUs_img"/>
+                <p className="aboutUs_text">{item.text}</p>
+              </li>
+            );
+          })}
+        </ul>
+        
+      </section>
+    </main>
+  );
 }
