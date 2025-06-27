24bf23678e49a3181fab1f16b33422068a39cd81
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-06-27 23:50:47 +0300

    added content and css for ourProgect page

--- src/app/ourprojects/page.js
+++ src/app/ourprojects/page.js
@@ -1,3 +1,22 @@
+import { ourProjectsData } from "@/constants";
+import { Button } from "@/components/Button";
+
 export default function Ourprojects() {
-  return <h1>Hello Projects</h1>;
+  return (
+    <section className="container ourProjects_section">
+      <p className="ourProjects_text">
+        Ми віримо, що кожен інтер’єр — це унікальна історія, а правильно
+        підібраний текстиль грає ключову роль у створенні гармонійного простору.
+      </p>
+      <ul className="ourProjects_list">
+        {ourProjectsData.map((item) => {
+          return (
+            <li key={item.label} className="ourProjects_item">
+              <Button text={item.label} isErrow={true} path={item.href} />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
---
+++ src/constants/ourProjectsData.js
@@ -0,0 +1,7 @@
+export const ourProjectsData = [
+    {src:"", label: "Прямі штори", href: "/"},
+    {src:"", label: "Римськи штори", href: "/"},
+    {src:"", label: "Жалюзі та рулонні штори", href: "/"},
+    {src:"", label: "Карнизи", href: "/"},
+    {src:"", label: "Домашній текстиль", href: "/"},
+]
