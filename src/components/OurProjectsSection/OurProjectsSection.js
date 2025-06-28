88d6c5be75daae9c3ef477dc5895055c93d1f65c
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-06-28 22:51:06 +0300

    added to ourProjectsData photos

---
+++ src/components/OurProjectsSection/OurProjectsSection.js
@@ -0,0 +1,5 @@
+export const OurProjectsSection =() => {
+    return (
+        <p>OurProjectsSection</p>
+    )
+};
--- src/constants/ourProjectsData.js
+++ src/constants/ourProjectsData.js
@@ -1,7 +1,13 @@
+import photo1 from "@/accets/img/ourProjects_photo1_x3.jpg";
+import photo2 from "@/accets/img/ourProjects_photo2_x3.jpg";
+import photo3 from "@/accets/img/ourProjects_photo3_x3.jpg";
+import photo4 from "@/accets/img/ourProjects_photo4_x3.jpg";
+import photo5 from "@/accets/img/ourProjects_photo5_x3.jpg";
+
 export const ourProjectsData = [
-    {src:"", label: "Прямі штори", href: "/"},
-    {src:"", label: "Римськи штори", href: "/"},
-    {src:"", label: "Жалюзі та рулонні штори", href: "/"},
-    {src:"", label: "Карнизи", href: "/"},
-    {src:"", label: "Домашній текстиль", href: "/"},
-]
+  { src: photo1, label: "Прямі штори", href: "/" },
+  { src: photo2, label: "Римськи штори", href: "/" },
+  { src: photo3, label: "Жалюзі та рулонні штори", href: "/" },
+  { src: photo4, label: "Карнизи", href: "/" },
+  { src: photo5, label: "Домашній текстиль", href: "/" },
+];
