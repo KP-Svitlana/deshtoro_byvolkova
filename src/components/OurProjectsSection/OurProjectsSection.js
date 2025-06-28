b4015fb0aeea3ee3ab701d6dbab7355f0811151e
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-06-28 23:12:06 +0300

    added base markup

--- src/components/OurProjectsSection/OurProjectsSection.js
+++ src/components/OurProjectsSection/OurProjectsSection.js
@@ -1,5 +1,22 @@
-export const OurProjectsSection =() => {
-    return (
-        <p>OurProjectsSection</p>
-    )
+import Image from "next/image";
+
+import { ourProjectsData } from "@/constants";
+import { Button } from "../Button";
+
+export const OurProjectsSection = () => {
+  return (
+    <section className="container ourProjectsSection_section">
+      <p className="ourProjectsSection_text">Наші проєкти</p>
+      <ul className="ourProjectsSection_list">
+        {ourProjectsData.map((item) => {
+          return (
+            <li key={item.label} className="ourProjectsSection_item">
+              <Image src={item.src} alt={item.label} />
+              <Button type="Button" text="Дивитись" path={item.href} />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
