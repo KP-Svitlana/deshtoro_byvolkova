3418bede08057a0ebec799c499c5b212b7e36253
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-06-29 14:29:30 +0300

    added css mobile and desktop

--- src/constants/ourProjectsData.js
+++ src/constants/ourProjectsData.js
@@ -3,11 +3,16 @@ import photo2 from "@/accets/img/ourProjects_photo2_x3.jpg";
 import photo3 from "@/accets/img/ourProjects_photo3_x3.jpg";
 import photo4 from "@/accets/img/ourProjects_photo4_x3.jpg";
 import photo5 from "@/accets/img/ourProjects_photo5_x3.jpg";
+import photo1_mob from "@/accets/img/ourProjects_photo1_x3_mobile.jpg";
+import photo2_mob from "@/accets/img/ourProjects_photo2_x3_mobile.jpg";
+import photo3_mob from "@/accets/img/ourProjects_photo3_x3_mobile.jpg";
+import photo4_mob from "@/accets/img/ourProjects_photo4_x3_mobile.jpg";
+import photo5_mob from "@/accets/img/ourProjects_photo5_x3_mobile.jpg";
 
 export const ourProjectsData = [
-  { src: photo1, label: "Прямі штори", href: "/" },
-  { src: photo2, label: "Римськи штори", href: "/" },
-  { src: photo3, label: "Жалюзі та рулонні штори", href: "/" },
-  { src: photo4, label: "Карнизи", href: "/" },
-  { src: photo5, label: "Домашній текстиль", href: "/" },
+  { src: photo1, src_m: photo1_mob, label: "Прямі штори", href: "/", key:"ourProj1" },
+  { src: photo2, src_m: photo2_mob, label: "Римськи штори", href: "/", key:"ourProj2" },
+  { src: photo3, src_m: photo3_mob, label: "Жалюзі та рулонні штори", href: "/", key:"ourProj3" },
+  { src: photo4, src_m: photo4_mob, label: "Карнизи", href: "/", key:"ourProj4" },
+  { src: photo5, src_m: photo5_mob, label: "Домашній текстиль", href: "/", key:"ourProj5" },
 ];
