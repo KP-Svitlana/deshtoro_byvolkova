b5ee1fb42a359cb8a9a3a11a9da43dca744acf59
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2025-07-29 21:43:59 +0300

    added ourServicesData

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import { navLinks, socLinks } from "./path";
 import {aboutUsData} from "./aboutUsData.js";
 import {ourProjectsData} from "./ourProjectsData";
+import {ourServicesData} from "./ourSErvicesData";
 
-export { navLinks, socLinks, aboutUsData, ourProjectsData };
+export { navLinks, socLinks, aboutUsData, ourProjectsData, ourServicesData };
---
+++ src/constants/ourServicesData.js
@@ -0,0 +1,23 @@
+import photo1 from "@/accets/img/ourServices_x3_photo1.jpg";
+import photo2 from "@/accets/img/ourServices_x3_photo2.jpg";
+import photo3 from "@/accets/img/ourServices_x3_photo3.jpg";
+import photo4 from "@/accets/img/ourServices_x3_photo4.jpg";
+
+export const ourServicesData = [
+  {
+    src: photo1,
+    label: "Пошиття",
+  },
+  {
+    src: photo2,
+    label: "Монтаж",
+  },
+  {
+    src: photo3,
+    label: "Заміри",
+  },
+  {
+    src: photo4,
+    label: "Навішування",
+  },
+];
