2f86fee611a80763f5dad6ce22d6bb8927495915
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2026-01-25 15:43:00 +0200

    added slider

--- src/components/OurServicesSection/OurServicesSection.js
+++ src/components/OurServicesSection/OurServicesSection.js
@@ -3,23 +3,30 @@ import Image from "next/image";
 import { ourServicesData } from "@/constants";
 import { Slider } from "../Slider";
 
-
 export const OurServicesSection = () => {
   return (
-    <section className="container section ourServicesSection_section">
-      <h2 className="section_title">наші послуги</h2>
-      <ul className="ourServicesSection_list">
-        {ourServicesData.map((item) => {
-          return (
-            <li key={item.label} className="ourServicesSection_item">
-              <Image src={item.src} alt={item.label} className="ourServices_img"/>
-              <div className="ourServices_wrap"><h3 className="ourServices_label">{item.label}</h3></div>
-              
-            </li>
-          );
-        })}
-      </ul>
-      <Slider data={ourServicesData}/>
+    <section className="container section">
+      <div className="ourServicesSection_section">
+        <h2 className="section_title">наші послуги</h2>
+        <ul className="ourServicesSection_list">
+          {ourServicesData.map((item) => {
+            return (
+              <li key={item.label} className="ourServicesSection_item">
+                <Image
+                  src={item.src}
+                  alt={item.label}
+                  className="ourServices_img"
+                />
+                <div className="ourServices_wrap">
+                  <h3 className="ourServices_label">{item.label}</h3>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <Slider data={ourServicesData} />
     </section>
   );
 };
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,8 +3,9 @@
 import { useState } from "react";
 
 import Image from "next/image";
-import arrowSvgBig from "@/accets/svg/arrowUp_big.svg";
 
+import arrowLeft from "@/accets/svg/sliderArrow_left.svg";
+import arrowRight from "@/accets/svg/sliderArrow_right.svg";
 
 export const Slider = ({ data }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,22 +19,28 @@ export const Slider = ({ data }) => {
   };
 
   return (
-    <div className="sliderContainer">
+    <div className="slider_container">
       <div className="slider">
-        {data.map((item, index) => (
-          <div
-            key={item.label}
-            className={`sliderItem ${index === currentIndex ? sliderItem.active : ""}`}
-          >
-            <Image src={item.src} alt={item.label}  />
-          </div>
-        ))}
+        {data.map((item, index) => {
+          const isActive = index === currentIndex;
+          return (
+            <div
+              key={item.label}
+              className={isActive ? "slider_item--active" : "slider_item"}
+            >
+              <Image src={item.src} alt={item.label} className="slider_img"/>
+              <div className="sliderLabel_wrap">
+                <h3 className="slider_label">{item.label}</h3>
+              </div>
+            </div>
+          );
+        })}
       </div>
       <button onClick={prevSlide}>
-        <Image src={arrowSvgBig} alt="Big arrow" />
+        <Image src={arrowLeft} alt="left arrow" />
       </button>
       <button onClick={nextSlide}>
-        <Image src={arrowSvgBig} alt="Big arrow" />
+        <Image src={arrowRight} alt="right arrow" />
       </button>
     </div>
   );
