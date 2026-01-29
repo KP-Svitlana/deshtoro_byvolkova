8dfb60638994d436253b37f7ae0546a144a8a415
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2026-01-29 16:14:05 +0200

    add img and base markup

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import { About } from "@/components/About";
 import { Hero } from "@/components/Hero";
 import { OurProjectsSection } from "@/components/OurProjectsSection";
 import { OurServicesSection } from "@/components/OurServicesSection";
+import { ReviewsSection } from "@/components/ReviewsSection/ReviewsSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <OurProjectsSection/>
       <OurServicesSection/>
+      <ReviewsSection/>
     </main>
   );
 }
---
+++ src/components/ReviewsSection/ReviewsSection.js
@@ -0,0 +1,8 @@
+
+export const ReviewsSection = () => {
+  return (
+    <section className="section container">
+      <p>ReviewsSection</p>
+    </section>
+  );
+};
