9599f34f3d39738df6edecf7ceb973b70147a48b
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2026-01-30 00:21:03 +0200

    added reviewsData

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,6 @@ import { navLinks, socLinks } from "./path";
 import {aboutUsData} from "./aboutUsData.js";
 import {ourProjectsData} from "./ourProjectsData";
 import {ourServicesData} from "./ourServicesData";
+import {reviewsData} from "./reviewsData";
 
-export { navLinks, socLinks, aboutUsData, ourProjectsData, ourServicesData };
+export { navLinks, socLinks, aboutUsData, ourProjectsData, ourServicesData, reviewsData  };
